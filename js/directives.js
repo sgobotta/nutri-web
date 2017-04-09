1061e5b1b76c70cb43557604c717892953d20f61
sgobotta <sgobotta@gmail.com>
2017-04-09 19:26:12 -0300

    View Updates: services individuales
    JS
    + se agregan directivas para cada view de servicio individual
    
    HTML
    + se modifica alimentary_plan para cumplir con especificaciones de cliente
    + se agregan las view de seis servicios individuales

--- js/directives.js
+++ js/directives.js
@@ -28,10 +28,95 @@ angular.module('App')
 	}
 })
 
+/* SERVICES */
 .directive('serviceAlimentaryPlanEs', function(){
 	return {
 		restrict: 'A',
 		templateUrl: '../templates/es/services/alimentary_plan.html'
+	}
+})
+.directive('serviceAlimentaryPlanEn', function(){
+	return {
+		restrict: 'A',
+		templateUrl: '../templates/en/services/alimentary_plan.html'
+	}
+})
+
+.directive('serviceAssistanceEs', function(){
+	return {
+		restrict: 'A',
+		templateUrl: '../templates/es/services/assistance.html'
+	}
+})
+.directive('serviceAssistanceEn', function(){
+	return {
+		restrict: 'A',
+		templateUrl: '../templates/en/services/assistance.html'
+	}
+})
+
+.directive('serviceExcersicePrescriptionEs', function(){
+	return {
+		restrict: 'A',
+		templateUrl: '../templates/es/services/excersice_prescription.html'
+	}
+})
+.directive('serviceExcersicePrescriptionEn', function(){
+	return {
+		restrict: 'A',
+		templateUrl: '../templates/en/services/excersice_prescription.html'
+	}
+})
+
+.directive('serviceMobileAppEs', function(){
+	return {
+		restrict: 'A',
+		templateUrl: '../templates/es/services/mobile_app.html'
+	}
+})
+.directive('serviceMobileAppEn', function(){
+	return {
+		restrict: 'A',
+		templateUrl: '../templates/en/services/mobile_app.html'
+	}
+})
+
+.directive('serviceCampusEs', function(){
+	return {
+		restrict: 'A',
+		templateUrl: '../templates/es/services/campus.html'
+	}
+})
+.directive('serviceCampusEn', function(){
+	return {
+		restrict: 'A',
+		templateUrl: '../templates/en/services/campus.html'
+	}
+})
+
+.directive('serviceGroupInteractionEs', function(){
+	return {
+		restrict: 'A',
+		templateUrl: '../templates/es/services/group_interaction.html'
+	}
+})
+.directive('serviceGroupInteractionEn', function(){
+	return {
+		restrict: 'A',
+		templateUrl: '../templates/en/services/group_interaction.html'
+	}
+})
+
+.directive('serviceArtTherapyEs', function(){
+	return {
+		restrict: 'A',
+		templateUrl: '../templates/es/services/art_therapy.html'
+	}
+})
+.directive('serviceArtTherapyEn', function(){
+	return {
+		restrict: 'A',
+		templateUrl: '../templates/en/services/art_therapy.html'
 	}
 })
 
